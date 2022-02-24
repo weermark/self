a9f90b60237405b6b414ebe7ce267e2253efbc73
weermark <79911758+weermark@users.noreply.github.com>
2022-02-24 13:21:52 +0800

    Create c1_1.cpp

---
+++ word_save/c1_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+	int a = 10, d;
+	float b = 3.3;
+	char c = 'A';
+	bool e = (5>3);
+	d = c;
+	
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+	cout << "c = " << c << endl;
+	cout << "d = " << d << endl;
+	cout << "e = " << e << endl;
+  return 0;
+}
